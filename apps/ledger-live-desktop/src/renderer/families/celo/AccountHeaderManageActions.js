8affa0515e9d7d714ea8e1865502a8d2d6f2f780
Andy Hass <ah@andrewhass.net>
2022-07-07 12:38:54 -0500

    fix: correct manage button icon color for celo

--- apps/ledger-live-desktop/src/renderer/families/celo/AccountHeaderManageActions.js
+++ apps/ledger-live-desktop/src/renderer/families/celo/AccountHeaderManageActions.js
@@ -1,20 +1,34 @@
 // @flow
-import { useCallback } from "react";
-import { useDispatch } from "react-redux";
+import React, { useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import type { Account } from "@ledgerhq/live-common/lib/types";
 
 import CryptoCurrencyIcon from "~/renderer/components/CryptoCurrencyIcon";
 import { openModal } from "~/renderer/actions/modals";
 import { useTranslation } from "react-i18next";
+import { themeSelector } from "~/renderer/actions/general";
+import theme from "~/renderer/styles/theme";
 
 type Props = {
   account: Account,
 };
 
+const Icon = (props: *) => {
+  const currentTheme = useSelector(themeSelector);
+
+  return (
+    <CryptoCurrencyIcon
+      {...props}
+      overrideColor={currentTheme === "dark" ? theme.colors.dark : theme.colors.white}
+    />
+  );
+};
+
 const AccountHeaderManageActions = ({ account }: Props) => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
+  const currentTheme = useSelector(themeSelector);
 
   const onClick = useCallback(() => {
     dispatch(
@@ -28,7 +42,7 @@ const AccountHeaderManageActions = ({ account }: Props) => {
     {
       key: "celo",
       onClick: onClick,
-      icon: CryptoCurrencyIcon,
+      icon: Icon,
       disabled: false,
       label: t("celo.manage.title"),
     },
